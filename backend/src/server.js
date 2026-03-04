7096d1b1df0cc0108e8958b1083811f8d51b64e0
Juan Martinez <juanmtzg584@gmail.com>
2026-03-04 16:01:30 -0600

    Listo para despliegue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 import express from "express";
 import { env } from "./config/env.js";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
 
 const app = express();
 
-app.use(express.json());
+//app.use(express.json());
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 
-app.listen(env.PORT, () => 
+//make ready for deployment
+if (env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
+  });
+}
+
+app.listen(env.PORT, () =>
   console.log(`Server listening on port ${env.PORT}`)
 );
