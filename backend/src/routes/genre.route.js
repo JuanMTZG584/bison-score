7459061fad834fbdff97595dec3a6051c65a5fac
Juan Martinez <juanmtzg584@gmail.com>
2026-05-05 18:17:07 -0600

    feat(genre): proteger endpoints de género

--- backend/src/routes/genre.route.js
+++ backend/src/routes/genre.route.js
@@ -6,18 +6,18 @@ import { requireAdmin } from "../middleware/requireAdmin.middleware.js";
 const router = express.Router();
 
 //READ Genres for Admin EJ: GET /api/genres?page=1&limit=10&search=RPG&is_active=true
-router.get("/", getAllGenres);
+router.get("/", protectRoute, requireAdmin, getAllGenres);
 
 //READ Genres for Users
 router.get("/options", getGenreOptions);
 
 //CREATE Genres (Admin) (protectRoute, requireAdmin)
-router.post("/", createGenre);
+router.post("/", protectRoute, requireAdmin, createGenre);
 
 //UPDATE Genres (Admin) (protectRoute, requireAdmin)
-router.put(("/:id"), updateGenre);
+router.put(("/:id"), protectRoute, requireAdmin, updateGenre);
 
 //PATCH Genres (Admin) (protectRoute, requireAdmin)
-router.patch(("/:id/toggle"), toggleGenreStatus);
+router.patch(("/:id/toggle"), protectRoute, requireAdmin, toggleGenreStatus);
 
 export default router;
